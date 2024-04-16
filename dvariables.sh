67901282580cdf3b4fa3765a7db0e56ed60cd889
Madhav reddy <reddydharani59@gmail.com>
2024-04-16 13:58:22 +0530

    variables declaration

--- dvariables.sh
+++ dvariables.sh
@@ -4,4 +4,4 @@ PERSON2=Suresh
 echo "$PERSON1 :Hi,I am $PERSON1"
 echo "$PERSON2: Hi,I am fine,How are you"
 echo "$PERSON1: Fine, what are you doing now"
-echo "$person2: Learing devops"
+echo "$PERSON: Learing devops"
