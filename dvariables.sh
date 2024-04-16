b23faf07fb5c8aa00381d151a025ddb52a7c2c61
Madhav reddy <reddydharani59@gmail.com>
2024-04-16 14:15:19 +0530

    variables declaration

--- dvariables.sh
+++ dvariables.sh
@@ -1,6 +1,7 @@
 #!bin/bash
-PERSON1=Ramesh
-PERSON2=Suresh
+# Declaring the variables
+PERSON1=$1
+PERSON2=$2
 echo "$PERSON1 :Hi,I am $PERSON1"
 echo "$PERSON2: Hi,I am fine,How are you"
 echo "$PERSON1: Fine, what are you doing now"
