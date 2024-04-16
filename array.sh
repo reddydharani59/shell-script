ea03341efad2d77cff39e94cb06617c6f9a85e58
Madhav reddy <reddydharani59@gmail.com>
2024-04-16 14:57:59 +0530

    example arrays

--- array.sh
+++ array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MOVIES=("mirchi,bahubali,bahubali2,pushpa,pushpa2")
+MOVIES=("mirchi" "bahubali" "bahubali2" "pushpa" "pushpa2")
 #size of this array is 5
 #index always starts with 0
 #list starts always with zero
