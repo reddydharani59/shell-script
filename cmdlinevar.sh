b4c5486fc8c5e2b8dead626b5871d86c7898c13a
Madhav reddy <reddydharani59@gmail.com>
2024-04-16 14:36:50 +0530

    dynamic arguments

---
+++ cmdlinevar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "please enter user name"
+read USERNAME #here username is variable
+echo "enter your password"
+read PASSWD
+echo "username is :" $USERNAME "and" "password is :" $PASSWD
