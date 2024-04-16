d74b678a4fb1f76bce53f32a25d959cb4a129759
Madhav reddy <reddydharani59@gmail.com>
2024-04-16 14:53:14 +0530

    example arrays

---
+++ array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MOVIES=("mirchi,bahubali,bahubali2,pushpa,pushpa2")
+#size of this array is 5
+#index always starts with 0
+#list starts always with zero
+
+echo "First Movie is :" ${MOVIES[0]}
+echo "Third Movie IS :" ${movies[3]}
