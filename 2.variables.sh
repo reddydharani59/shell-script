299fdd784e2ee314c900be95c6663cb01ecae640
Madhav reddy <reddydharani59@gmail.com>
2024-04-16 13:29:38 +0530

    variables declaration

--- 2.variables.sh
+++ 2.variables.sh
@@ -2,5 +2,5 @@
 echo "Ramesh :Hi,I am ramesh"
 echo "Suresh : Hi,I am fine,How are you"
 echo "Ramesh : Fine, what are you doing now"
-echo :Suresh: Learing devops"
+echo "Suresh: Learing devops"
 
---
+++ dvariables.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1 :Hi,I am $PERSON1"
+echo "$PERSON2: Hi,I am fine,How are you"
+echo "$PERSON1: Fine, what are you doing now"
+echo "$person2: Learing devops"
