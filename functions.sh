ffe25312872ae46b68bfbea42915b16ccaaf0d2e
Madhav reddy <reddydharani59@gmail.com>
2024-04-18 15:14:49 +0530

    functions uasge

--- functions.sh
+++ functions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 USER=$(id -u)
-TIMESTAMP=$(date +F-%H-%M-%S)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE()
 {
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then 
     echo "$2....Failure"
     else
