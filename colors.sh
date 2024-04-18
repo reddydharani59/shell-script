2768fedd0a2e8ed7842fa976f292f891fd5d8ed5
Madhav reddy <reddydharani59@gmail.com>
2024-04-18 15:34:36 +0530

    functions uasge

---
+++ colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+USER=${id -u}
+if [ $USER -ne 0 ]
+then 
+    echo -e " ...$R you won't have access"
+    exit 1 
+
+else 
+    echo -e "$2....$G success, $y you are super user"
+fi 
--- installation.sh
+++ installation.sh
@@ -2,8 +2,10 @@
 USER=$(id -u)
 if[ $USER -nt 0]
 then
-echo "get super user access"
+    echo "get super user access"
+    exit 1
 else
-echo "you have access"
+    echo "you have access"
 fi
 dnf install mysql -y
+
