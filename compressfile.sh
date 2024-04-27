abb4f6214a289517c9afa0059db6ea2019e92ca4
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 13:01:33 +0530

    counting numbers

--- compressfile.sh
+++ compressfile.sh
@@ -4,7 +4,8 @@ echo "please enter source file"
 read SOURCE_FILE
 #SOURCE_FILE=/home/ec2-user/shell-script/text.txt
 DESTINATION_FOLDER=/tmp/zip
-BACKUP_FILE=$SOURCE_FILE.$TIMESTAMP
-tar cfz $SOURCE_FILE.tar.gz $BACKUP_FILE
+
+tar cfz $SOURCE_FILE.tar.gz $SOURCE_FILE
+BACKUP_FILE=$SOURCE_FILE.tar.gz.$TIMESTAMP
 DESTINATION_FILE=$(cp $BACKUP_FILE $DESTINATION_FOLDER) 
 echo " FILE COMPRESSED "
