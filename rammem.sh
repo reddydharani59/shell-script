8e869a9c5dfbd362fffcba4b303c5e49c793b0af
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 19:43:14 +0530

    counting numbers

--- rammem.sh
+++ rammem.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 RAM_USAGE=$(free -m)
 
-TOTAL_MEM=$(echo $RAM_USAGE | grep Mem: | awk -F " " '{print $2}')
+TOTAL_MEM=$($RAM_USAGE | grep Mem: | awk -F " " '{print $2}')
 
-USED_MEM=$(echo $RAM_USAGE | grep Mem: | awk -F " " '{print $3}')
+USED_MEM=$($RAM_USAGE | grep Mem: | awk -F " " '{print $3}')
  echo "Total memory is  : $TOTAL_MEM and used mem: $USED_MEM"
 
  
