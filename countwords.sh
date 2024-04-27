760998b1034645a3e94b545674efdaa46e0c5e0d
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 10:28:18 +0530

    counting numbers

---
+++ countwords.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+COL
+ROW
+DATA
+while IFS=' ' read -r fields 
+do
+COL+=("{fields[0]}")
+DATA+=("{fields[1]}")
+done <<<(cat /home/ec2-user/txt.sh)
---
+++ txt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+name age
+alice 21
+ryan 30
