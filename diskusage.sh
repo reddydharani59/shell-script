8ff3e1c8e0c52f56afd9ab755f1ec1f7b1cbe58d
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 18:13:00 +0530

    counting numbers

--- diskusage.sh
+++ diskusage.sh
@@ -1,12 +1,22 @@
+
 #!/bin/bash
-DISK_USAGE=$(df -h | grep xfs)
-THRESHOLD=10
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=6
+MESSAGE=""
+
 while IFS= read -r line
-do 
-    USAGE=$(echo $line | awk -F " " '{pritn $6F}' | cut -d "%" -f1 )
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    if [ $USAGE -ge $THRESHOLD ]
+    if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-     echo "$Folder is more than $THRESHOLD, current usage: $USAGE"
-     fi
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
+    fi
 done <<< $DISK_USAGE
+
+echo -e "Message: $MESSAGE"
+
+echo "$MESSAGE" | mail -s "Disk Usage Alert" info@joindevops.com
+
+# echo "body" | mail -s "subject" to-address
