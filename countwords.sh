20a38da675ae76274de9b15576cab8a49858dfbe
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 10:32:48 +0530

    counting numbers

--- countwords.sh
+++ countwords.sh
@@ -2,8 +2,9 @@
 COL=""
 ROW=""
 DATA=""
+FILE=/home/ec2-user/shell-script/txt.sh
 while IFS=' ' read -r fields 
 do
 COL+=("{fields[0]}")
 DATA+=("{fields[1]}")
-done <<< (cat /home/ec2-user/shell-script/txt.sh)
+done <<< $FILE
