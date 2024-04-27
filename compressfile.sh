ee433df983226cb06da753ec1c3d88674b50cc87
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 13:37:42 +0530

    counting numbers

--- compressfile.sh
+++ compressfile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 TIMESTAMP=$(date +%F-%H-%M-S)
-BACKUP_FILE="BACKUP_FILE".$TIMESTAMP
+
 echo "please enter source file"
 read SOURCE_FILE
 echo "please enter destination directory"
 read DIRECTORY_FILE
 #SOURCE_FILE=/home/ec2-user/shell-script/text.txt
-tar cfz $BACKUP_FILE $SOURCE_FILE
+BACKUP_FILE.$TIMESTAMP=$(tar cfz $BACKUP_FILE.tar.gz $SOURCE_FILE)
 
-cp -rf $BACKUP_FILE $DESTINATION_FOLDER 
+cp -rf "$BBACKUP_FILE.$TIMESTAMPACKUP_FILE" "$DESTINATION_FOLDER" 
 echo " FILE COMPRESSED "
