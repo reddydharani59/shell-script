bf79cbd416925008c0b0036f24ebff156c04fd5d
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 14:57:27 +0530

    counting numbers

--- compressfile.sh
+++ compressfile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-date=$(date +"%d%m%y")
+date=$(date +%f-"%d%m%y")
 backup_file="backup_$date.tar.gz"
  echo "enter source directory"
  read source_dir
  echo "enter destination directory"
  read destination_dir
   
-  tar -cvzf "$source_dir.tar.gz" "$source_dir"
+  tar -cvzf "$backup_file.tar.gz" "$source_dir"
   echo $?
-  cp -rp "$source_dir.tar.gz" "$destination_dir"
+  cp -rp "$backup_file.tar.gz" "$destination_dir"
     echo $?
     echo "backup filecompressed succefully"
