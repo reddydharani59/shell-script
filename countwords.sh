7629a57bc5f262b8367610424be8909ba0cc6a50
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 10:36:35 +0530

    counting numbers

--- countwords.sh
+++ countwords.sh
@@ -5,7 +5,7 @@ DATA=""
 FILE=/home/ec2-user/shell-script/txt.sh
 while IFS=' ' read -r fields 
 do
-COL+=("{fields[0]}")
-DATA+=("{fields[1]}")
-echo "$COL $DATA"
+COL+=("{$fields[0]}")
+DATA+=("{$fields[1]}")
+echo " $COL $DATA "
 done <<< $FILE
