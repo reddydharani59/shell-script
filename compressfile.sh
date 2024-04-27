c1a8bbab61c3b0566c29bb12189be3ed1d5ebfeb
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 12:33:52 +0530

    counting numbers

--- compressfile.sh
+++ compressfile.sh
@@ -2,6 +2,5 @@
 TIMESTAMP=$(date +%F-%H-%M-S)
 SOURCE_FILE=/home/ec2-user/shell-script/text.txt
 DESTINATION_FOLDER=/tmp/zip
-FILE=$(tar cfz $SOURCE_FILE.tar.gz $SOURCE_FILE) 
-DESTINATION_FOLDER=$FILE
+FILE=$(tar cfz $SOURCE_FILE.tar.gz $SOURCE_FILE) @>>$DESTINATION_FOLDER
 echo " FILE COMPRESSED "
