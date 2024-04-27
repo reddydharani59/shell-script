e009a76f4cef29f8330f8b29aa70f8751ce3df85
Madhav reddy <reddydharani59@gmail.com>
2024-04-27 14:13:17 +0530

    counting numbers

--- compressfile.sh
+++ compressfile.sh
@@ -6,7 +6,7 @@ read SOURCE_FILE
 echo "please enter destination directory"
 read DESTINATION_FILE
 #SOURCE_FILE=/home/ec2-user/shell-script/text.txt
-tar cfz "$BACKUP_FILE.tar.gz" "$SOURCE_FILE"
+tar cfz "$BACKUP_FILE.tar.gz" "txt1.sh"
 
 cp -rf "$BBACKUP_FILE.tar.gz" "$DESTINATION_FILE" 
 echo " FILE COMPRESSED "
